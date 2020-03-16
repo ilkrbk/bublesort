c9ee8275f47d97dd7b5a7ffc6389b4e557131d39
ilkrbk <ilkrbk@gmail.com>
2020-03-16 18:49:05 +0200

    first commit

---
+++ BubleSort/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BubleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
