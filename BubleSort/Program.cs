89abb1a933a916984bf43a1498b2bb51a1435cd6
ilkrbk <ilkrbk@gmail.com>
2020-03-16 19:03:37 +0200

    final buble sort

--- BubleSort/Program.cs
+++ BubleSort/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BubleSort
 {
@@ -6,7 +7,26 @@ namespace BubleSort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> array = new List<int>() {4, 28, 21, 85, 35, 30, 1, 2, 5, 3, 67};
+            for (int i = 0; i < array.Count - 1; i++)
+            {
+                for (int j = 0; j < array.Count - i - 1; j++)
+                {
+                    // > по возростанию
+                    // < по убыванию
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+
+            for (int i = 0; i < array.Count; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
         }
     }
 }
